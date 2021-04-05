decred/btcd

addrmgr: Remove DNS lookups from address manager.
This removes the need to perform DNS lookups in the address manager.
It also modifies address manager network addresses to store the
address type when constructed, and canonicalizes the IP so that it
is stored in a standard format.

server.go
@@ -358,6 +358,36 @@ func (ps *peerState) forAllPeers(closure func(sp *serverPeer)) {
 	ps.forAllOutboundPeers(closure)
 }
 
+// hostToNetAddress parses and returns an address manager network address given
+// a hostname in a supported format (IPv4, IPv6, TORv2).  If the hostname
+// cannot be immediately converted from a known address format, it will be
+// resolved using a DNS lookup function. If it cannot be resolved, an error is
+// returned.
+func (cfg *config) hostToNetAddress(host string, port uint16, services wire.ServiceFlag) (*addrmgr.NetAddress, error) {
+	addrmgrServices := addrmgr.ServiceFlag(services)
+	networkID, addrBytes, err := addrmgr.HostToBytes(host)
+	if err != nil {
+		return nil, err
+	}
+	if networkID != addrmgr.UnknownAddressType {
+		// Since the host has been successfully decoded, there is no need to
+		// perform a DNS lookup.
+		now := time.Unix(time.Now().Unix(), 0)
+		return addrmgr.NewNetAddressByType(networkID, addrBytes, port,
+			now, addrmgrServices)
+	}
+
+	ips, err := cfg.dcrdLookup(host)
+	if err != nil {
+		return nil, err
+	}
+	if len(ips) == 0 {
+		return nil, fmt.Errorf("no addresses found for %s", host)
+	}
+	na := addrmgr.NewNetAddress(ips[0], port, addrmgrServices)
+	return na, nil
+}
+
 // ResolveLocalAddress picks the best suggested network address from available
 // options, per the network interface key provided. The best suggestion, if
 // found, is added as a local address.
@@ -380,8 +410,7 @@ func (ps *peerState) ResolveLocalAddress(netType addrmgr.NetAddressType, addrMgr
 	}
 
 	addLocalAddress := func(bestSuggestion string, port uint16, services wire.ServiceFlag) {
-		na, err := addrMgr.HostToNetAddress(bestSuggestion, port,
-			addrmgr.ServiceFlag(services))
+		na, err := cfg.hostToNetAddress(bestSuggestion, port, services)
 		if err != nil {
 			amgrLog.Errorf("unable to generate network address using host %v: "+
 				"%v", bestSuggestion, err)
@@ -2170,8 +2199,7 @@ func newPeerConfig(sp *serverPeer) *peer.Config {
 		},
 		NewestBlock: sp.newestBlock,
 		HostToNetAddress: func(host string, port uint16, services wire.ServiceFlag) (*wire.NetAddress, error) {
-			address, err := sp.server.addrManager.HostToNetAddress(host, port,
-				addrmgr.ServiceFlag(services))
+			address, err := cfg.hostToNetAddress(host, port, services)
 			if err != nil {
 				return nil, err
 			}
@@ -3003,11 +3031,10 @@ func (s *server) querySeeders(ctx context.Context) {
 			// seeded addresses.  In the incredibly rare event that the lookup
 			// fails after it just succeeded, fall back to using the first
 			// returned address as the source.
-			srcAddr := wireToAddrmgrNetAddress(addrs[0])
-			srcIPs, err := dcrdLookup(seeder)
-			if err == nil && len(srcIPs) > 0 {
-				const httpsPort = 443
-				srcAddr = addrmgr.NewNetAddress(srcIPs[0], httpsPort, 0)
+			const httpsPort = 443
+			srcAddr, err := cfg.hostToNetAddress(seeder, httpsPort, 0)
+			if err != nil {
+				srcAddr = wireToAddrmgrNetAddress(addrs[0])
 			}
 
 			addresses := wireToAddrmgrNetAddresses(addrs)
@@ -3332,7 +3359,7 @@ func setupRPCListeners() ([]net.Listener, error) {
 // decred network type specified by chainParams.  Use start to begin accepting
 // connections from peers.
 func newServer(ctx context.Context, listenAddrs []string, db database.DB, chainParams *chaincfg.Params, dataDir string) (*server, error) {
-	amgr := addrmgr.New(cfg.DataDir, dcrdLookup)
+	amgr := addrmgr.New(cfg.DataDir)
 	services := defaultServices
 
 	var listeners []net.Listener
@@ -3842,8 +3869,7 @@ func initListeners(ctx context.Context, params *chaincfg.Params, amgr *addrmgr.A
 				eport = uint16(port)
 			}
 
-			na, err := amgr.HostToNetAddress(host, eport,
-				addrmgr.ServiceFlag(services))
+			na, err := cfg.hostToNetAddress(host, eport, services)
 			if err != nil {
 				srvrLog.Warnf("Not adding %s as externalip: %v", sip, err)
 				continue
@@ -3889,7 +3915,7 @@ func addrStringToNetAddr(addr string) (net.Addr, error) {
 	// Attempt to look up an IP address associated with the parsed host.
 	// The dcrdLookup function will transparently handle performing the
 	// lookup over Tor if necessary.
-	ips, err := dcrdLookup(host)
+	ips, err := cfg.dcrdLookup(host)
 	if err != nil {
 		return nil, err
 	}
@@ -3944,8 +3970,7 @@ func addLocalAddress(addrMgr *addrmgr.AddrManager, addr string, services wire.Se
 			addrMgr.AddLocalAddress(netAddr, addrmgr.BoundPrio)
 		}
 	} else {
-		na, err := addrMgr.HostToNetAddress(host, uint16(port),
-			addrmgr.ServiceFlag(services))
+		na, err := cfg.hostToNetAddress(host, uint16(port), services)
 		if err != nil {
 			return err
 		}

server_test.go
@@ -1,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"reflect"
+	"testing"
+
+	"github.com/decred/dcrd/addrmgr/v2"
+	"github.com/decred/dcrd/wire"
+)
+
+// TestHostToNetAddress ensures that HostToNetAddress behaves as expected
+// given valid and invalid host name arguments.
+func TestHostToNetAddress(t *testing.T) {
+	// Define a hostname that will cause a lookup to be performed using the
+	// lookupFunc provided to the address manager instance for each test.
+	const hostnameForLookup = "hostname.test"
+	const services = wire.SFNodeNetwork
+	const addrmgrServices = addrmgr.ServiceFlag(services)
+
+	tests := []struct {
+		name       string
+		host       string
+		port       uint16
+		lookupFunc func(host string) ([]net.IP, error)
+		wantErr    bool
+		want       *addrmgr.NetAddress
+	}{
+		{
+			name:       "valid onion address",
+			host:       "a5ccbdkubbr2jlcp.onion",
+			port:       8333,
+			lookupFunc: nil,
+			wantErr:    false,
+			want: addrmgr.NewNetAddress(
+				net.ParseIP("fd87:d87e:eb43:744:208d:5408:63a4:ac4f"), 8333,
+				addrmgrServices),
+		},
+		{
+			name:       "invalid onion address",
+			host:       "0000000000000000.onion",
+			port:       8333,
+			lookupFunc: nil,
+			wantErr:    true,
+			want:       nil,
+		},
+		{
+			name: "unresolvable host name",
+			host: hostnameForLookup,
+			port: 8333,
+			lookupFunc: func(host string) ([]net.IP, error) {
+				return nil, fmt.Errorf("unresolvable host %v", host)
+			},
+			wantErr: true,
+			want:    nil,
+		},
+		{
+			name: "not resolved host name",
+			host: hostnameForLookup,
+			port: 8333,
+			lookupFunc: func(host string) ([]net.IP, error) {
+				return nil, nil
+			},
+			wantErr: true,
+			want:    nil,
+		},
+		{
+			name: "resolved host name",
+			host: hostnameForLookup,
+			port: 8333,
+			lookupFunc: func(host string) ([]net.IP, error) {
+				return []net.IP{net.ParseIP("127.0.0.1")}, nil
+			},
+			wantErr: false,
+			want: addrmgr.NewNetAddress(net.ParseIP("127.0.0.1"), 8333,
+				addrmgrServices),
+		},
+		{
+			name:       "valid ip address",
+			host:       "12.1.2.3",
+			port:       8333,
+			lookupFunc: nil,
+			wantErr:    false,
+			want: addrmgr.NewNetAddress(net.ParseIP("12.1.2.3"), 8333,
+				addrmgrServices),
+		},
+	}
+
+	for _, test := range tests {
+		cfg := &config{
+			lookup:      test.lookupFunc,
+			onionlookup: test.lookupFunc,
+		}
+		result, err := cfg.hostToNetAddress(test.host, test.port, services)
+		if test.wantErr == true && err == nil {
+			t.Errorf("%q: expected error but one was not returned", test.name)
+		}
+		if !reflect.DeepEqual(result, test.want) {
+			t.Errorf("%q: unexpected result -- got %v, want %v", test.name,
+				result, test.want)
+		}
+	}
+}

sefbkn/dcrd

server.go
@@ -359,6 +359,36 @@ func (ps *peerState) forAllPeers(closure func(sp *serverPeer)) {
 	ps.forAllOutboundPeers(closure)
 }
 
+// hostToNetAddress parses and returns an address manager network address given
+// a hostname in a supported format (IPv4, IPv6, TORv2).  If the hostname
+// cannot be immediately converted from a known address format, it will be
+// resolved using a DNS lookup function. If it cannot be resolved, an error is
+// returned.
+func (cfg *config) hostToNetAddress(host string, port uint16, services wire.ServiceFlag) (*addrmgr.NetAddress, error) {
+	addrmgrServices := addrmgr.ServiceFlag(services)
+	networkID, addrBytes, err := addrmgr.ParseHost(host)
+	if err != nil {
+		return nil, err
+	}
+	if networkID != addrmgr.UnknownAddressType {
+		// Since the host has been successfully decoded, there is no need to
+		// perform a DNS lookup.
+		now := time.Unix(time.Now().Unix(), 0)
+		return addrmgr.NewNetAddressByType(networkID, addrBytes, port,
+			now, addrmgrServices)
+	}
+
+	ips, err := cfg.dcrdLookup(host)
+	if err != nil {
+		return nil, err
+	}
+	if len(ips) == 0 {
+		return nil, fmt.Errorf("no addresses found for %s", host)
+	}
+	na := addrmgr.NewNetAddressIPPort(ips[0], port, addrmgrServices)
+	return na, nil
+}
+
 // ResolveLocalAddress picks the best suggested network address from available
 // options, per the network interface key provided. The best suggestion, if
 // found, is added as a local address.
@@ -381,8 +411,7 @@ func (ps *peerState) ResolveLocalAddress(netType addrmgr.NetAddressType, addrMgr
 	}
 
 	addLocalAddress := func(bestSuggestion string, port uint16, services wire.ServiceFlag) {
-		na, err := addrMgr.HostToNetAddress(bestSuggestion, port,
-			addrmgr.ServiceFlag(services))
+		na, err := cfg.hostToNetAddress(bestSuggestion, port, services)
 		if err != nil {
 			amgrLog.Errorf("unable to generate network address using host %v: "+
 				"%v", bestSuggestion, err)
@@ -2188,11 +2217,8 @@ func newPeerConfig(sp *serverPeer) *peer.Config {
 			OnWrite:          sp.OnWrite,
 			OnNotFound:       sp.OnNotFound,
 		},
-		NewestBlock: sp.newestBlock,
-		HostToNetAddress: func(host string, port uint16, services wire.ServiceFlag) (*addrmgr.NetAddress, error) {
-			return sp.server.addrManager.HostToNetAddress(host, port,
-				addrmgr.ServiceFlag(services))
-		},
+		NewestBlock:       sp.newestBlock,
+		HostToNetAddress:  cfg.hostToNetAddress,
 		Proxy:             cfg.Proxy,
 		UserAgentName:     userAgentName,
 		UserAgentVersion:  userAgentVersion,
@@ -3019,11 +3045,10 @@ func (s *server) querySeeders(ctx context.Context) {
 			// seeded addresses.  In the incredibly rare event that the lookup
 			// fails after it just succeeded, fall back to using the first
 			// returned address as the source.
-			srcAddr := wireToAddrmgrNetAddress(addrs[0])
-			srcIPs, err := dcrdLookup(seeder)
-			if err == nil && len(srcIPs) > 0 {
-				const httpsPort = 443
-				srcAddr = addrmgr.NewNetAddressIPPort(srcIPs[0], httpsPort, 0)
+			const httpsPort = 443
+			srcAddr, err := cfg.hostToNetAddress(seeder, httpsPort, 0)
+			if err != nil {
+				srcAddr = wireToAddrmgrNetAddress(addrs[0])
 			}
 			addresses := wireToAddrmgrNetAddresses(addrs)
 			s.addrManager.AddAddresses(addresses, srcAddr)
@@ -3346,7 +3371,7 @@ func setupRPCListeners() ([]net.Listener, error) {
 // decred network type specified by chainParams.  Use start to begin accepting
 // connections from peers.
 func newServer(ctx context.Context, listenAddrs []string, db database.DB, chainParams *chaincfg.Params, dataDir string) (*server, error) {
-	amgr := addrmgr.New(cfg.DataDir, dcrdLookup)
+	amgr := addrmgr.New(cfg.DataDir)
 	services := defaultServices
 
 	var listeners []net.Listener
@@ -3855,8 +3880,7 @@ func initListeners(ctx context.Context, params *chaincfg.Params, amgr *addrmgr.A
 				eport = uint16(port)
 			}
 
-			na, err := amgr.HostToNetAddress(host, eport,
-				addrmgr.ServiceFlag(services))
+			na, err := cfg.hostToNetAddress(host, eport, services)
 			if err != nil {
 				srvrLog.Warnf("Not adding %s as externalip: %v", sip, err)
 				continue
@@ -3920,7 +3944,7 @@ func addrStringToNetAddr(addr string) (net.Addr, error) {
 	// Attempt to look up an IP address associated with the parsed host.
 	// The dcrdLookup function will transparently handle performing the
 	// lookup over Tor if necessary.
-	ips, err := dcrdLookup(host)
+	ips, err := cfg.dcrdLookup(host)
 	if err != nil {
 		return nil, err
 	}
@@ -3970,8 +3994,7 @@ func addLocalAddress(addrMgr *addrmgr.AddrManager, addr string, services wire.Se
 			addrMgr.AddLocalAddress(netAddr, addrmgr.BoundPrio)
 		}
 	} else {
-		netAddr, err := addrMgr.HostToNetAddress(host, uint16(port),
-			addrmgr.ServiceFlag(services))
+		netAddr, err := cfg.hostToNetAddress(host, uint16(port), services)
 		if err != nil {
 			return err
 		}

server_test.go
@@ -1,0 +1,108 @@
+// Copyright (c) 2021 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"net"
+	"reflect"
+	"testing"
+
+	"github.com/decred/dcrd/addrmgr/v2"
+	"github.com/decred/dcrd/wire"
+)
+
+// TestHostToNetAddress ensures that HostToNetAddress behaves as expected
+// given valid and invalid host name arguments.
+func TestHostToNetAddress(t *testing.T) {
+	// Define a hostname that will cause a lookup to be performed using the
+	// lookupFunc provided to the address manager instance for each test.
+	const hostnameForLookup = "hostname.test"
+	const services = wire.SFNodeNetwork
+	const addrmgrServices = addrmgr.ServiceFlag(services)
+
+	tests := []struct {
+		name       string
+		host       string
+		port       uint16
+		lookupFunc func(host string) ([]net.IP, error)
+		wantErr    bool
+		want       *addrmgr.NetAddress
+	}{
+		{
+			name:       "valid onion address",
+			host:       "a5ccbdkubbr2jlcp.onion",
+			port:       8333,
+			lookupFunc: nil,
+			wantErr:    false,
+			want: addrmgr.NewNetAddressIPPort(
+				net.ParseIP("fd87:d87e:eb43:744:208d:5408:63a4:ac4f"), 8333,
+				addrmgrServices),
+		},
+		{
+			name:       "invalid onion address",
+			host:       "0000000000000000.onion",
+			port:       8333,
+			lookupFunc: nil,
+			wantErr:    true,
+			want:       nil,
+		},
+		{
+			name: "unresolvable host name",
+			host: hostnameForLookup,
+			port: 8333,
+			lookupFunc: func(host string) ([]net.IP, error) {
+				return nil, fmt.Errorf("unresolvable host %v", host)
+			},
+			wantErr: true,
+			want:    nil,
+		},
+		{
+			name: "not resolved host name",
+			host: hostnameForLookup,
+			port: 8333,
+			lookupFunc: func(host string) ([]net.IP, error) {
+				return nil, nil
+			},
+			wantErr: true,
+			want:    nil,
+		},
+		{
+			name: "resolved host name",
+			host: hostnameForLookup,
+			port: 8333,
+			lookupFunc: func(host string) ([]net.IP, error) {
+				return []net.IP{net.ParseIP("127.0.0.1")}, nil
+			},
+			wantErr: false,
+			want: addrmgr.NewNetAddressIPPort(net.ParseIP("127.0.0.1"), 8333,
+				addrmgrServices),
+		},
+		{
+			name:       "valid ip address",
+			host:       "12.1.2.3",
+			port:       8333,
+			lookupFunc: nil,
+			wantErr:    false,
+			want: addrmgr.NewNetAddressIPPort(net.ParseIP("12.1.2.3"), 8333,
+				addrmgrServices),
+		},
+	}
+
+	for _, test := range tests {
+		cfg := &config{
+			lookup:      test.lookupFunc,
+			onionlookup: test.lookupFunc,
+		}
+		result, err := cfg.hostToNetAddress(test.host, test.port, services)
+		if test.wantErr == true && err == nil {
+			t.Errorf("%q: expected error but one was not returned", test.name)
+		}
+		if !reflect.DeepEqual(result, test.want) {
+			t.Errorf("%q: unexpected result -- got %v, want %v", test.name,
+				result, test.want)
+		}
+	}
+}